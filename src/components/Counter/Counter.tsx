840b1babdcdda121e43367f3e5ce1feb9add136e
Sergey Lemeshko <lemexaxa@gmail.com>
2024-12-18 17:26:51 +0300

    поэкспериментировал со счетчиком

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./Counter.css";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [counterCount, setCounterCount] = useState("counter-blue");
+
+  const plusCount = () => {
+    setCounter(counter + 1);
+  };
+
+  const minusCount = () => {
+    setCounter(counter - 1);
+  };
+
+    useEffect(() => {
+    if (counter % 2 === 0) {
+      setCounterCount("counter-red");
+    } else {
+      setCounterCount("counter-blue");
+    }
+  }, [counter]);
+
+  return (
+    <>
+      <div className={counterCount}>{counter}</div>
+      <div className="buttons">
+        <button onClick={plusCount}>+</button>
+        <button onClick={minusCount}>-</button>
+      </div>
+    </>
+  )
+}
